08ed81697a38ea33741a01705950f1940db3ac9b
iammostak <mdmostafijur85@gmail.com>
2022-12-30 00:30:27 +0530

    uploading and downloading done

--- src/routes/ImportCSV.jsx
+++ src/routes/ImportCSV.jsx
@@ -1,6 +1,83 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import {
+   Button,
+   Heading,
+   HStack,
+   Input,
+   Text,
+   useToast,
+   VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+const tax_rates = { 0: 5, 1: 8, 2: 12 };
+
+const addTaxes = (data) => {
+   data = data.map((item, index) => {
+      item = item.split(",");
+
+      if (index === 0) {
+         item.push("tax");
+         item = item.join(",");
+         return item;
+      } else {
+         let amount = +item[1],
+            item_type = +item[2];
+         let tax_amount = amount * (tax_rates[item_type] / 100);
+
+         if (item_type > 2) {
+            item.push("NA");
+            item = item.join(",");
+            return item;
+         } else {
+            item.push(tax_amount.toFixed(2));
+            item = item.join(",");
+            return item;
+         }
+      }
+   });
+
+   data = data.join("\r\n");
+
+   const blob = new Blob([data], { type: "text/csv" });
+   const file = new File([blob], "result.csv", { type: "text/csv" });
+
+   return file;
+};
 
 function ImportCSV() {
+   const [inputFile, setInputFile] = useState({});
+   const toast = useToast();
+   const [url, setUrl] = useState("");
+
+   const handleFile = () => {
+      if (!inputFile.name) {
+         toast({
+            title: "Please select a invoice.csv file",
+            status: "warning",
+            position: "top",
+            duration: 3000,
+            isClosable: true,
+         });
+         return;
+      }
+      const reader = new FileReader();
+
+      reader.onload = async (event) => {
+         let file = addTaxes(event.target.result.split("\r\n"));
+
+         setUrl(URL.createObjectURL(file));
+         toast({
+            title: "file uploaded successfully",
+            status: "success",
+            position: "top",
+            duration: 3000,
+            isClosable: true,
+         });
+      };
+
+      reader.readAsText(inputFile);
+   };
+
    return (
       <VStack my={7} justify={"stretch"} spacing={5}>
          <Heading
@@ -16,6 +93,42 @@ function ImportCSV() {
          >
             import invoice.csv
          </Heading>
+         <Text
+            fontFamily={"Helvetica"}
+            letterSpacing={0.5}
+            align={"center"}
+            w={700}
+            pb={4}
+            color={"blackAlpha.700"}
+         >
+            Upload a .csv file, File should have s.no, amount, item_type in it
+            to get the texes.
+            <br /> Also you will be getting the taxes of those amounts which
+            have item_type as 0, 1 or 2 other than that taxes will be "NA"
+         </Text>
+         <HStack>
+            <Input
+               py={1}
+               type="file"
+               accept=".csv"
+               border={"2px solid"}
+               borderColor={"blue.500"}
+               onChange={(event) => setInputFile(event.target.files[0])}
+            />
+            <Button px={7} colorScheme={"blue"} onClick={handleFile}>
+               Upload
+            </Button>
+         </HStack>
+         <Button
+            w={447}
+            disabled={!url}
+            as={"a"}
+            href={url}
+            colorScheme={"green"}
+            download="result"
+         >
+            Download
+         </Button>
       </VStack>
    );
 }
