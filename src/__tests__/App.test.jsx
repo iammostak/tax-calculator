5dd12c2d040fcec37e4753b4aba1d55d2e4dadc7
iammostak <mdmostafijur85@gmail.com>
2023-01-01 16:14:29 +0530

    new test cases added

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -4,6 +4,7 @@ import DataTable from "../routes/DataTable";
 import { invoices } from "../db.json";
 import App from "../components/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import ImportCSV from "../routes/ImportCSV";
 
 afterEach(cleanup);
 
@@ -20,9 +21,9 @@ test("routes are working correctly", () => {
    fireEvent.click(home);
    setTimeout(() => {
       expect(getByTestId("data-table")).toBeInTheDocument();
+      fireEvent.click(upload);
    }, 1000);
 
-   fireEvent.click(upload);
    setTimeout(() => {
       expect(getByTestId("upload-csv")).toBeInTheDocument();
    }, 1000);
@@ -70,4 +71,38 @@ test("tax calculate correctly", () => {
          `(Tax Rate : ${tax_rates[item_type]}%)`
       );
    });
+});
+
+test("conditional rendering working correctly in csv page", () => {
+   const { getByTestId } = render(<ImportCSV />);
+
+   const input_file = getByTestId("input-file");
+   const upload_btn = getByTestId("upload-btn");
+
+   const content = `
+      s.no, amount, item_type\r 
+      1, 1200, 0\r
+      2, 25000, 1\r
+      3, 35545, 2\r
+      4, 1500, 1\r
+      5, 23456, 3
+   `;
+
+   let blob = new Blob([content], { type: "text/csv" });
+   let file = new File([blob], "invoice.csv", { type: "text/csv" });
+
+   fireEvent.change(input_file, { target: { files: [file] } });
+
+   fireEvent.click(upload_btn);
+
+   setTimeout(() => {
+      const calculate_btn = getByTestId("calculate-btn");
+      expect(calculate_btn).toBeInTheDocument();
+      fireEvent.click(calculate_btn);
+   }, 1000);
+
+   setTimeout(() => {
+      const download_form = getByTestId("download-form");
+      expect(download_form).toBeInTheDocument();
+   }, 1000);
 });
